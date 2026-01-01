3f05732327d6fcfe2cd695c317a394a30d8b1b19
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-01-01 13:44:56 +0100

    add upload dependencies

---
+++ lib/features/upload/init_upload_dependencies.dart
@@ -0,0 +1,17 @@
+import 'package:car_renting/core/di/injection_container.dart';
+import 'package:car_renting/features/upload/cubit/upload_cubit.dart';
+import 'package:car_renting/features/upload/repositories/cloudinary_repository.dart';
+import 'package:car_renting/features/upload/service/upload_service.dart';
+import 'package:dio/dio.dart';
+
+Future<void> initUploadDependencies() async {
+  serviceLocator.registerLazySingleton<CloudinaryRepository>(
+    () => CloudinaryRepository(Dio()),
+  );
+
+  serviceLocator.registerFactory<UploadService>(() => UploadService());
+
+  serviceLocator.registerFactory<UploadCubit>(
+    () => UploadCubit(repository: serviceLocator<CloudinaryRepository>()),
+  );
+}
