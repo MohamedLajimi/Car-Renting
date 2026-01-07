ecb3cc24727f13b5ca61be7cdf73ca8a35d16ce7
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-01-07 17:53:13 +0100

    refactor(navigation): define car management routes and refactor auth routes

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,4 +1,7 @@
-import 'package:car_renting/features/auth/auth_routes.dart';
+import 'package:car_renting/features/auth/routes/auth_routes.dart';
+import 'package:car_renting/features/car-management/routes/car_management_routes.dart';
 import 'package:go_router/go_router.dart';
 
-final goRouter = GoRouter(routes: [...AuthRoutes.routes]);
+final goRouter = GoRouter(
+  routes: [...AuthRoutes.routes, CarManagementRoutes.route],
+);
--- lib/features/auth/routes/auth_routes.dart
+++ lib/features/auth/routes/auth_routes.dart
@@ -1,8 +1,8 @@
 import 'package:car_renting/core/di/injection_container.dart';
-import 'package:car_renting/core/routes/app_routes.dart';
 import 'package:car_renting/features/auth/cubits/forgot_password_cubit/forgot_password_cubit.dart';
 import 'package:car_renting/features/auth/cubits/login_cubit/login_cubit.dart';
 import 'package:car_renting/features/auth/cubits/signup_cubit/signup_cubit.dart';
+import 'package:car_renting/features/auth/routes/auth_routes_names.dart';
 import 'package:car_renting/features/auth/screens/signup_screen.dart';
 import 'package:car_renting/features/auth/screens/forgot_password_screen.dart';
 import 'package:car_renting/features/auth/screens/login_screen.dart';
@@ -14,34 +14,34 @@ import 'package:go_router/go_router.dart';
 class AuthRoutes {
   static List<RouteBase> routes = [
     GoRoute(
-      name: AppRoutes.root,
-      path: AppRoutes.rootPath,
+      name: AuthRoutesNames.root,
+      path: AuthRoutesNames.rootPath,
       builder: (context, state) => const RootLoadingScreen(),
     ),
     GoRoute(
-      name: AppRoutes.onboarding,
-      path: AppRoutes.onboardingPath,
+      name: AuthRoutesNames.onboarding,
+      path: AuthRoutesNames.onboardingPath,
       builder: (context, state) => const OnboardingScreen(),
     ),
     GoRoute(
-      name: AppRoutes.login,
-      path: AppRoutes.loginPath,
+      name: AuthRoutesNames.login,
+      path: AuthRoutesNames.loginPath,
       builder: (context, state) => BlocProvider(
         create: (context) => serviceLocator<LoginCubit>(),
         child: const LoginScreen(),
       ),
     ),
     GoRoute(
-      name: AppRoutes.createAccount,
-      path: AppRoutes.createAccountPath,
+      name: AuthRoutesNames.createAccount,
+      path: AuthRoutesNames.createAccountPath,
       builder: (context, state) => BlocProvider(
         create: (context) => serviceLocator<SignupCubit>(),
         child: const SignupScreen(),
       ),
     ),
     GoRoute(
-      name: AppRoutes.forgotPassword,
-      path: AppRoutes.forgotPasswordPath,
+      name: AuthRoutesNames.forgotPassword,
+      path: AuthRoutesNames.forgotPasswordPath,
       builder: (context, state) => BlocProvider(
         create: (context) => serviceLocator<ForgotPasswordCubit>(),
         child: const ForgotPasswordScreen(),
