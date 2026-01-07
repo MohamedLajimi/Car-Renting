1e647999a23f9bd938164873ae7f44c37b348d73
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-01-07 17:53:41 +0100

    define structured pagination response

--- lib/core/utils/pagination_repsonse.dart
+++ lib/core/utils/pagination_repsonse.dart
@@ -4,13 +4,15 @@ class PaginationResponse<T> extends Equatable {
   final List<T> items;
   final int totalCount;
   final bool hasNextPage;
+  final dynamic lastDoc;
 
   const PaginationResponse({
     required this.items,
     required this.totalCount,
     required this.hasNextPage,
+    required this.lastDoc
   });
 
   @override
-  List<Object?> get props => [items, totalCount, hasNextPage];
+  List<Object?> get props => [items, totalCount, hasNextPage, lastDoc];
 }
